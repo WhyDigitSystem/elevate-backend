2b5fc555d87aa9740de265d9eb31a44df055991f
Justin Aravinth <justin@whydigit.in>
2025-09-22 14:52:13 +0530

    correction

--- src/main/java/com/ebooks/elevate/service/MonthlyProcessServiceImpl.java
+++ src/main/java/com/ebooks/elevate/service/MonthlyProcessServiceImpl.java
@@ -127,10 +127,15 @@ public class MonthlyProcessServiceImpl implements MonthlyProcessService {
 					previousYearActualVO.setAccountCode(monthlyProcessDetailsVO2.getElGlCode());
 					previousYearActualVO.setAccountName(monthlyProcessDetailsVO2.getElGl());
 					previousYearActualVO.setNatureOfAccount(monthlyProcessDetailsVO2.getNatureOfAccount());
-					if (monthlyProcessDetailsVO2.getNatureOfAccount().equals("Cr")) {
-						previousYearActualVO.setAmount(
-								monthlyProcessDetailsVO2.getClosingBalance().multiply(BigDecimal.valueOf(-1)));
-					} else {
+					if (!monthlyProcessVO.getMainGroup().equals("Balance Sheet Schedule")) {
+						if (monthlyProcessDetailsVO2.getNatureOfAccount().equals("Cr")) {
+							previousYearActualVO.setAmount(
+									monthlyProcessDetailsVO2.getClosingBalance().multiply(BigDecimal.valueOf(-1)));
+						} else {
+							previousYearActualVO.setAmount(monthlyProcessDetailsVO2.getClosingBalance());
+						}
+					}
+					else {
 						previousYearActualVO.setAmount(monthlyProcessDetailsVO2.getClosingBalance());
 					}
 					int quater = quaterMonthService.getQuaterMonthDetails(monthlyProcessDTO.getYearType(),
